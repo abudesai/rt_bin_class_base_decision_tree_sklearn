5aac0839ca915681f5e7f0ea17a9a78f73a503fb
Abu Desai <abhyuday.desai@gmail.com>
2022-12-21 13:06:37 -0800

    test changed to predict; added json inference; user 1000 in dockerfile; nginx chown in dockerfile

--- app/inference_app.py
+++ app/inference_app.py
@@ -2,7 +2,8 @@
 # https://github.com/aws/amazon-sagemaker-examples/blob/main/advanced_functionality/scikit_bring_your_own/container/decision_trees/predictor.py
 
 import io
-import pandas as pd
+import json
+import numpy as np, pandas as pd
 import flask
 import traceback
 import sys
@@ -42,9 +43,53 @@ def ping():
     return flask.Response(response=response, status=status, mimetype="application/json")
 
 
-
 @app.route("/infer", methods=["POST"])
 def infer():
+    """Do an inference on a single batch of data. In this sample server, we take data as a JSON object, convert
+    it to a pandas data frame for internal use and then convert the predictions back to JSON.
+    """
+    # Convert from CSV to pandas
+    if flask.request.content_type == "application/json":
+        req_data_dict = json.loads(flask.request.data.decode("utf-8"))
+        data = pd.DataFrame.from_records(req_data_dict["instances"])
+        print(f"Invoked with {data.shape[0]} records")
+    else:
+        return flask.Response(
+            response="This endpoint only supports application/json data",
+            status=415,
+            mimetype="text/plain",
+        )
+
+    # Do the prediction
+    try:
+        # Do the prediction
+        print(f"Invoked with {data.shape[0]} records")
+        print(data)
+        predictions = model_server.predict_to_json(data)
+        return {
+            "success": True,
+            "predictions": predictions,
+        }
+
+    except Exception as err:
+        # Write out an error file. This will be returned as the failureReason to the client.
+        trc = traceback.format_exc()
+        error_msg = "Exception during inference: " + str(err) + "\n" + trc
+        with open(failure_path, "w") as s:
+            s.write(error_msg)
+        # Printing this causes the exception to be in the training job logs, as well.
+        print("Exception during inference: " + str(err) + "\n" + trc, file=sys.stderr)
+        # A non-zero exit code causes the training job to be marked as Failed.
+        response = json.dumps({"error": str(err)})
+        return flask.Response(
+            response=response,
+            status=400,
+            mimetype="text/plain",
+        )
+
+
+@app.route("/infer_file", methods=["POST"])
+def infer_file():
     """Do an inference on a single batch of data. In this sample server, we take data as CSV, convert
     it to a pandas data frame for internal use and then convert the predictions back to CSV (which really
     just means one prediction per line, since there's a single column.
